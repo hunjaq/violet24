226746c69f265afb4fedd4e0f30907780420aad3
patwarne <94197273+patwarne@users.noreply.github.com>
2024-02-03 16:25:36 -0500

    Update App.jsx

--- hackapp/src/App.jsx
+++ hackapp/src/App.jsx
@@ -1,18 +1,49 @@
-import logo from './logo.svg';
 import React from 'react';
 import './App.css';
-// import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
-    <div className="App">
-        <header className="App-header">
-          <h1>
-            Interview App
-          </h1>
-        </header>
-    </div>
-
+    <Box 
+      className="App" 
+      display="flex" 
+      flexDirection="column" 
+      justifyContent="space-between"
+      alignItems="center" 
+      height="100vh"
+      style={{ marginTop: '20px' }}
+      flexGrow={1}
+    >
+      <header className="Chat-Gpt">
+        <h1>Interview App</h1>
+      </header>
+      <Card sx={{ minWidth: 500, flexGrow: 1, marginBottom: '10px' }}>
+        <CardContent>
+          <Typography variant="h5" component="div">
+            Chat-GPT 
+          </Typography>
+          <Typography variant="body2">
+            Chat-GPT response
+          </Typography>
+        </CardContent>
+      </Card>
+      <Card sx={{ minWidth: 500, marginBottom: '10px' }}>
+        <TextField
+          id="outlined-textarea"
+          label="Multiline Placeholder"
+          placeholder="Placeholder"
+          multiline
+          variant="standard" // Change from 'outlined' to 'standard'
+          sx={{ minWidth: 500 }}
+        />
+        <Button variant="contained">Button</Button>
+      </Card>
+    </Box>
   );
 }
 
